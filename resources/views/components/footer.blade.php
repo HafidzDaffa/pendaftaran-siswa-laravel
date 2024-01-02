c27d803075f77d093c5bad88fcaac40b9ac7792f
Randi Prananda <randibprananda@gmail.com>
2024-01-02 20:25:08 +0700

    add footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="w-full bg-primary flex justify-between py-[36px] px-[118px] items-center text-[24px]">
+    <div class="flex flex-col gap-y-6">
+        <h1 class="font-bold">Logo</h1>
+        <p>text</p>
+    </div>
+    <div class="flex flex-col gap-y-6">
+        <h1 class="font-bold">Kontak Kami</h1>
+        <p>WA</p>
+        <p>Email</p>
+    </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#DCDAB6",
+            }
+        },
+        fontFamily: {
+            inter: "Inter"
+        }
+    },
+    plugins: [],
 }
 
